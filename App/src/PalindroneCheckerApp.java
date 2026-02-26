8884f244328aeec797897357a73b581f5628e932
Jashwanth <ja9285@srmist.edu.in>
2026-02-26 16:10:29 +0530

    [Jashwanth] Added [UC1-WelcomePage]

---
+++ App/src/PalindroneCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindroneCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrone Checker App ");
+        System.out.println(" Version : 1.0 ");
+        System.out.println("=================================");
+
+    }
+}
